c68587c09b1b6cd292aa9bfd2685bb0cf086f909
eliveli <60069112+eliveli@users.noreply.github.com>
2022-06-30 04:18:04 +0900

    Refactor : Get the character index where text length is 11 or 12 bytes

--- src/utils/getTextLength.ts
+++ src/utils/getTextLength.ts
@@ -1,6 +1,6 @@
 export default function getTextLength(str: string) {
   let len = 0;
-  let charIndexAs12bytes = 0;
+  let neededCharIndex = 0;
   for (let i = 0; i < str.length; i += 1) {
     if (escape(str.charAt(i)).length === 6) {
       len += 1;
@@ -9,8 +9,15 @@ export default function getTextLength(str: string) {
 
     // get the character index where the text length is 12 bytes
     if (len === 12) {
-      charIndexAs12bytes = i;
+      neededCharIndex = i;
+    }
+
+    // get the character index where the text length is 11 bytes
+    // - in this case the character index where text length is 12 bytes doesn't exist
+    // - 13 bytes === 11 bytes (at the previous char index) + 2 bytes (current char length)
+    if (len === 13 && neededCharIndex === 0) {
+      neededCharIndex = i - 1;
     }
   }
-  return [len, charIndexAs12bytes];
+  return [len, neededCharIndex];
 }
