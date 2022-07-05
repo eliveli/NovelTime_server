2c7eff5929ee0efd723e60fe091233e554751056
eliveli <60069112+eliveli@users.noreply.github.com>
2022-07-06 04:14:38 +0900

    Feat : Get talk title and Set comment info

--- src/services/contents/contents.utils.ts
+++ src/services/contents/contents.utils.ts
@@ -2,5 +2,6 @@ export const query = {
   geUserId: " SELECT userId FROM user WHERE userName = (?) ",
   getWritings: " SELECT * FROM writing WHERE userId = (?) ",
   getNovelTitleAndImg: " SELECT novelTitle, novelImg FROM novelInfo WHERE novelId = (?) ",
+  getTalkTitle: " SELECT writingTitle FROM writing WHERE writingId = (?) ",
   getComments: " SELECT * FROM comment WHERE userId = (?) ",
 };
--- src/services/contents/getComments.ts
+++ src/services/contents/getComments.ts
@@ -6,10 +6,61 @@ type Comment = {
   commentId: string;
   writingId: string;
   userId: string;
+  novelTitle: string;
   createDate: string;
   commentContent: string;
   originalCommentIdForReComment: string; // if it is not "" empty string, get the info for it
 };
+
+function getTalkTitle(talkId: string) {
+  return new Promise<string>(async (resolve) => {
+    await pool
+      .getConnection()
+      .then((connection) => {
+        connection
+          .query(query.getTalkTitle, talkId)
+          .then((data) => {
+            const { writingTitle } = data[0];
+            resolve(writingTitle);
+
+            // When done with the connection, release it.
+            connection.release();
+          })
+
+          .catch((err) => {
+            console.log(err);
+            connection.release();
+          });
+      })
+      .catch((err) => {
+        console.log(`not connected due to error: ${err}`);
+      });
+  });
+}
+
+async function setCommentInfo(comment: Comment) {
+  const {
+    commentId,
+    writingId,
+    userId,
+    novelTitle,
+    createDate,
+    commentContent,
+    originalCommentIdForReComment,
+  } = comment;
+
+  const talkId = writingId;
+  const talkTitle = await getTalkTitle(talkId);
+
+  return {
+    commentId,
+    commentContent,
+    createDate,
+    talkId,
+    talkTitle,
+    novelTitle,
+  };
+}
 
 // for userPageHome page : extractComments(comments, 4)
 // then return the four comments
@@ -28,10 +79,18 @@ export default function getComments(userId: string) {
       .then((connection) => {
         connection
           .query(query.getComments, userId)
-          .then((data) => {
+          .then(async (data) => {
             const comments = data.slice(0, data.length);
             const selectedComments = extractComments(comments, 4);
-            resolve(selectedComments);
+
+            const commentsSet = [];
+
+            for (const comment of selectedComments) {
+              const commentSet = await setCommentInfo(comment);
+              commentsSet.push(commentSet);
+            }
+
+            resolve({ commentsSet });
 
             // When done with the connection, release it.
             connection.release();
