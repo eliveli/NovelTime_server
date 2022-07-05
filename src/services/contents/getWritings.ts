8cecc6079bf764c283228adf8f0fad2001b9571b
eliveli <60069112+eliveli@users.noreply.github.com>
2022-07-05 22:42:17 +0900

    Refactor : Devide writings

--- src/services/contents/getWritings.ts
+++ src/services/contents/getWritings.ts
@@ -1,5 +1,44 @@
+/* eslint-disable no-restricted-syntax */
 import pool from "../../configs/db";
 import { query } from "./contents.utils";
+
+type Writings = {
+  writingId: string;
+  userId: string;
+  createDate: string;
+  writingTitle: string;
+  writingImg: string;
+  writingDesc: string;
+  novelId: string;
+  likeNO: number;
+  commentNO: number;
+  talkOrRecommend: "T" | "R";
+};
+function divideWritings(writings: Writings[], number: number, order = 1) {
+  const talks = [];
+  const recommends = [];
+  for (const writing of writings) {
+    if (writing.talkOrRecommend === "T") {
+      talks.push(writing);
+    }
+    if (writing.talkOrRecommend === "R") {
+      recommends.push(writing);
+    }
+
+    // when requesting writings from userPageHome page, "order" is always 1
+    // otherwise requesting from userPageWritings page, "order" will be 1 or bigger one
+    //   because if an user clicks the "more" button writings in next order will be required
+    if (talks.length >= number * order && recommends.length >= number * order) {
+      break;
+    }
+  }
+
+  // set the lists as requested after setting the arrays of talks and recommends
+  const newTalks = talks.slice(number * (order - 1), number * order);
+  const newRecommends = recommends.slice(number * (order - 1), number * order);
+
+  return [newTalks, newRecommends];
+}
 
 export default function getWritings(userId: string) {
   return new Promise<any>(async (resolve) => {
@@ -10,7 +49,9 @@ export default function getWritings(userId: string) {
           .query(query.getWritings, userId)
           .then((data) => {
             const writings = data.slice(0, data.length);
-            resolve(writings);
+            const dividedWritings = divideWritings(writings, 4);
+            resolve(dividedWritings);
+
             // When done with the connection, release it.
             connection.release();
           })
