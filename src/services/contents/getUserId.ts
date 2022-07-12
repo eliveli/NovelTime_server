109b3caf650428d0211face2fcbef7ef5fe494d2
eliveli <60069112+eliveli@users.noreply.github.com>
2022-07-12 20:10:28 +0900

    Refactor : Change the logic as simple as using async-await and try-catch

--- src/services/contents/getUserId.ts
+++ src/services/contents/getUserId.ts
@@ -1,28 +1,30 @@
 import pool from "../../configs/db";
 import { query } from "./contents.utils";
 
-export default function getUserId(userName: string) {
-  return new Promise<string>(async (resolve) => {
-    await pool
-      .getConnection()
-      .then((connection) => {
-        connection
-          .query(query.getUserId, userName)
-          .then((data) => {
-            const { userId } = data[0];
-            resolve(userId as string);
+type UserId = { userId: string };
+export default async function getUserId(userName: string) {
+  let userIdFromDB = ""; // to avoid undefined return error of es-lint
+  try {
+    const connection = await pool.getConnection();
 
-            // When done with the connection, release it.
-            connection.release();
-          })
+    try {
+      const data = (await connection.query(query.getUserId, userName)) as UserId[];
 
-          .catch((err) => {
-            console.log(err);
-            connection.release();
-          });
-      })
-      .catch((err) => {
-        console.log(`not connected due to error: ${err}`);
-      });
-  });
+      if (!data) {
+        throw new Error("user does not exist in DB");
+      }
+      const { userId } = data[0];
+
+      // When done with the connection, release it.
+      await connection.release();
+
+      userIdFromDB = userId;
+    } catch (err) {
+      console.log("error: ", err);
+      await connection.release();
+    }
+  } catch (err) {
+    console.log("not connected due to error: ", err);
+  }
+  return userIdFromDB;
 }
