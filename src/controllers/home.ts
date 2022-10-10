a6bf35071ade8257f730257367c4defa22bbc3a6
eliveli <60069112+eliveli@users.noreply.github.com>
2022-10-11 01:23:48 +0900

    Fix : Update home controller as adding service functions

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -1,16 +1,34 @@
 import { RequestHandler } from "express";
 
 import dotenv from "dotenv";
-import writingHomeService from "../services/writing/home";
+import writingHomeService from "../services/home";
 
 dotenv.config();
 
 export const homeController: RequestHandler = (async (req, res) => {
   try {
     const talkList = await writingHomeService.getWritings("T");
+
+    const talk = await writingHomeService.getUserRankOfWritings("T", "Create");
+    const comment = await writingHomeService.getUserRankOfWritings("C", "Create");
+    const likeReceivedOfTalk = await writingHomeService.getUserRankOfWritings("T", "ReceiveLike");
+    const talkUserRank = { talk, comment, likeReceived: likeReceivedOfTalk };
+
     const recommendList = await writingHomeService.getWritings("R");
 
-    res.json("server connected");
+    const recommend = await writingHomeService.getUserRankOfWritings("R", "Create");
+    const likeReceivedOfRecommend = await writingHomeService.getUserRankOfWritings(
+      "R",
+      "ReceiveLike",
+    );
+    const recommendUserRank = { recommend, likeReceived: likeReceivedOfRecommend };
+
+    res.json({
+      talkList,
+      talkUserRank,
+      recommendList,
+      recommendUserRank,
+    });
   } catch (error: any) {
     console.log("failed to get content in homeController :", error);
     res.status(500).end();
