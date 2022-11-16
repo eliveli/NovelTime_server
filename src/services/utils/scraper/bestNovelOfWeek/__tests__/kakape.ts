de7271b595c747fa013fbbf953ca825116477fea
eliveli <60069112+eliveli@users.noreply.github.com>
2022-11-17 02:17:25 +0900

    Test : Test checkNovelInDB function as a success case

--- src/services/utils/scraper/bestNovelOfWeek/__tests__/kakape.ts
+++ src/services/utils/scraper/bestNovelOfWeek/__tests__/kakape.ts
@@ -1,6 +1,23 @@
-import weeklyKakape from "../kakape";
+import puppeteer from "puppeteer";
+import weeklyKakape, { checkNovelInDB } from "../kakape";
 
 jest.setTimeout(500000);
-it("case to run properly :", async () => {
-  await weeklyKakape();
+
+// it("case to run a weekly scraper properly :", async () => {
+//   await weeklyKakape();
+// });
+
+it("case to add a novel platform kakao page into a novel row that is already in DB with removing duplicate novel rows or updating novel platforms:", async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  page.setDefaultTimeout(500000);
+  await page.goto("https://page.kakao.com/content/60524067?tab_type=about");
+
+  const novelInfo = {
+    novelTitle: "폭군의 흑화를 막는 법",
+    novelAuthor: "한여온",
+    novelUrl: "page.kakao.com/content/53555298",
+  };
+  const novelId = await checkNovelInDB(page, novelInfo);
+  console.log("novelId:", novelId);
 });
