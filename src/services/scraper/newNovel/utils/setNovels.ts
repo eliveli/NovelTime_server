4c615424a4e4793340b6260239cc7a65bb0b0985
eliveli <60069112+eliveli@users.noreply.github.com>
2022-12-20 13:01:10 +0900

    Fix : Change characters from NO to No in this function
    as changing other modules

--- src/services/scraper/newNovel/utils/setNovels.ts
+++ src/services/scraper/newNovel/utils/setNovels.ts
@@ -4,38 +4,38 @@ import { NovelPlatform } from "../../utils/types";
 
 export default async function setNovels(
   page: puppeteer.Page,
-  novelNOandPageNO: {
-    currentNovelNO: number;
-    totalNovelNO: number;
-    totalPageNO: number[] | number;
+  novelNoAndPageNo: {
+    currentNovelNo: number;
+    totalNovelNo: number;
+    totalPageNo: number[] | number;
   },
   novelPlatform: NovelPlatform,
   currentNovelUrl: string,
 ) {
-  const { currentNovelNO, totalNovelNO, totalPageNO } = novelNOandPageNO;
+  const { currentNovelNo, totalNovelNo, totalPageNo } = novelNoAndPageNo;
 
-  let novelNO = currentNovelNO;
+  let novelNo = currentNovelNo;
 
   // 작품 상세페이지 조회 반복
-  while (totalNovelNO >= novelNO) {
+  while (totalNovelNo >= novelNo) {
     console.log(
-      `currentNovelNO: ${novelNO}, totalNovelNO: ${totalNovelNO}, totalPageNO: `,
-      totalPageNO,
+      `currentNovelNo: ${novelNo}, totalNovelNo: ${totalNovelNo}, totalPageNo: `,
+      totalPageNo,
     );
 
     try {
       await addOrUpdateNovelInDB(page, currentNovelUrl, novelPlatform);
 
-      novelNO += 1; // 작품 번호 +1
+      novelNo += 1; // 작품 번호 +1
 
-      if (novelNO % 100 === 0) break; // 작품 100번째 마다 loop 탈출. for 시크릿창 여닫기
+      if (novelNo % 100 === 0) break; // 작품 100번째 마다 loop 탈출. for 시크릿창 여닫기
     } catch (err) {
-      console.log(err, `\n 현재작품: ${novelNO}, 마지막작품: ${totalNovelNO}`);
+      console.log(err, `\n 현재작품: ${novelNo}, 마지막작품: ${totalNovelNo}`);
       // 에러 발생 시 해당 작품은 통과. 시크릿창 여닫으며 다음 작품으로 넘어감
-      novelNO += 1; // 작품 번호 +1
+      novelNo += 1; // 작품 번호 +1
       break;
     }
   }
 
-  return novelNO; // 시크릿창을 닫고 열면서 다음에 조회할 작품 번호 표시
+  return novelNo; // 시크릿창을 닫고 열면서 다음에 조회할 작품 번호 표시
 }
