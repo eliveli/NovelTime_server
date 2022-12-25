c5c01bc21fed5039f5f48f1def74084da4689b71
eliveli <60069112+eliveli@users.noreply.github.com>
2022-12-26 00:01:47 +0900

    Fix : Stop scraper when there is no more url to read
    in this case the urls are less than total novel number.
    this can occur when the scraper failed previously to read some url from the novel list page for some reason

--- src/services/scraper/newNovel/utils/setNovels.ts
+++ src/services/scraper/newNovel/utils/setNovels.ts
@@ -7,33 +7,44 @@ export default async function setNovels(
   novelNoAndPageNo: {
     currentNovelNo: number;
     totalNovelNo: number;
-    totalPageNo: number[] | number; // for ridi, series
+    totalNovelNoList: number[]; // for ridi
+    totalPageNo: number[] | number; // number[] for ridi, number for series
   },
   novelPlatform: NovelPlatform,
   novelUrls: string[],
 ) {
-  const { currentNovelNo, totalNovelNo, totalPageNo } = novelNoAndPageNo;
+  const { currentNovelNo, totalNovelNo, totalNovelNoList, totalPageNo } = novelNoAndPageNo;
 
   let novelNo = currentNovelNo;
 
   // 작품 상세페이지 조회 반복
   while (totalNovelNo >= novelNo) {
     console.log(
-      `currentNovelNo: ${novelNo}, totalNovelNo: ${totalNovelNo}, totalPageNo: `,
-      totalPageNo,
+      `currentNovelNo: ${novelNo}, totalNovelNo: ${totalNovelNo}, ${
+        totalNovelNoList.length !== 0 ? `totalNovelNoList: ${String(totalNovelNoList)}, ` : ""
+      } totalPageNo: ${String(totalPageNo)}`,
     );
 
     try {
+      // 스크랩할 소설 수 보다 읽어온 url 수가 작을 때
+      if (!novelUrls[novelNo - 1]) {
+        throw Error("읽을 url이 더 이상 없음");
+      }
+
       const novelId = await addOrUpdateNovelInDB(page, novelUrls[novelNo - 1], novelPlatform);
       console.log("novelId: ", novelId);
 
       novelNo += 1; // 작품 번호 +1
 
       if (novelNo % 100 === 0) break; // 작품 100번째 마다 loop 탈출. for 시크릿창 여닫기
-    } catch (err) {
+    } catch (err: any) {
       console.log(err, `\n 현재작품: ${novelNo}, 마지막작품: ${totalNovelNo}`);
       // 에러 발생 시 해당 작품은 통과. 시크릿창 여닫으며 다음 작품으로 넘어감
       novelNo += 1; // 작품 번호 +1
+
+      if (err.message === "읽을 url이 더 이상 없음") {
+        novelNo = totalNovelNo + 1; // 스크래퍼 종료 조건
+      }
       break;
     }
   }
