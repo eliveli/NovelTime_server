fa6acc56a368c8222a9f992e355f16e6794a786f
eliveli <60069112+eliveli@users.noreply.github.com>
2022-12-28 11:50:57 +0900

    Comment : Note for when failing to run a scraper

--- src/services/scraper/newNovel/__tests__/scrapeNewNovels.ts
+++ src/services/scraper/newNovel/__tests__/scrapeNewNovels.ts
@@ -6,27 +6,29 @@ import setNovels from "../utils/setNovels";
 // increase time set to prevent scraper from stopping in the middle
 jest.setTimeout(60000000);
 
-// 추후 하기 !!
-// 아래 장르 필터 값을 string 에서 number로 바꾸고 이 값을 사용하는 다른 함수들 변경!!!!
-// 그리고 나중에 utils 폴더에 분리
-//
+// 스크래퍼 실행이 지연/중단/실패할 때 //
+// [상황]
+//  - 목록페이지에서 url을 읽어올 때 page down을 해도
+//    dom이 화면에 늦게 그려져 필요 element를 적시에 읽지 못함
+//  - 로그인 화면에서 일부 글자가 빠진 채로 입력됨
+// [대처]
+//  - 다른 프로세스(인터넷 브라우저, DBeaver 등) 종료
+//    : 메모리 용량 줄이기 위함. (참고: 작업관리자-성능-메모리)
+//  - 실행 중 인풋 넣지 않기(마우스, 키보드 등)
+//    : 현재 작업을 방해할 수 있음
+//  - <중요> 해당 플랫폼에서 설렉터 변경 여부 확인, 코드의 설렉터 변경 <중요>
 
-// 스크래퍼 실행 시 다른 작업(인터넷 브라우저, DBeaver 등) 다 종료한 상태에서 스크래퍼만 실행하기.
-//  그렇지 않으면 메모리 용량 차지를 많이 해서(참고: 작업관리자-성능-메모리)
-//   목록페이지에서 url을 읽어올 때 page down을 하더라도
-//   dom 로드가 느려 element를 제때 읽어오지 못함
-
-// it("run a new scraper for ridi :", async () => {
-//   await newScraper("리디북스", genreFilter.ridi.F1);
-// });
+it("run a new scraper for a certain amount of novels for series :", async () => {
+  await newScraper("네이버 시리즈", genreFilter.series.F, 1);
+});
 
 // it("run a new scraper for a certain amount of novels for ridi:", async () => {
 //   await newScraper("리디북스", genreFilter.ridi.F1, 1);
 // });
 
-it("run a new scraper for kakape :", async () => {
-  await newScraper("카카오페이지", genreFilter.kakape.RF, 1);
-});
+// it("run a new scraper for a certain amount of novels for kakape :", async () => {
+//   await newScraper("카카오페이지", genreFilter.kakape.RF, 1);
+// });
 
 // it("test setNovels :", async () => {
 //   const novelNoAndPageNo = {
